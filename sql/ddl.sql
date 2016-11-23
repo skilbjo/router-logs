f2f3cbd3d58bae4ce8aa45778546af693ea6067c
John Skilbeck <john.skilbeck@gmail.com>
2016-11-23 00:24:46 -0800

    code complete

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -3,10 +3,26 @@ begin;
 commit;
 
 begin;
+  drop table if exists log;
+  drop table if exists log_staging;
+
+  create table log_staging (
+    id              serial,
+    date            date,
+    time            time,
+    device          text,
+    syslog_tag      text,
+    program         text,
+    log             text
+  );
+
   create table log (
-    id    serial,
-    date  date,
-    host  text,
-    log   text
+    id              serial,
+    date            date,
+    time            time,
+    device          text,
+    syslog_tag      text,
+    program         text,
+    log             text
   );
 commit;
---
+++ sql/staging.sql
@@ -0,0 +1,22 @@
+begin;
+  insert into log (
+    date, time, device, syslog_tag, program, log
+  )
+  with candidate_records as (
+    select staging.*
+    from log
+      right join log_staging staging on log.date = staging.date
+                                    and log.time = staging.time
+                                    and log.device = staging.device
+                                    and log.syslog_tag = staging.syslog_tag
+                                    and log.program = staging.program
+                                    and log.log = staging.log
+    where
+      log.id is null
+  )
+  select
+    date, time, device, syslog_tag, program, log
+  from
+    candidate_records
+  ;
+commit;
