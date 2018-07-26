fc644f778739156b003ae97ee006df958c59a600
John Skilbeck <john.skilbeck@gmail.com>
2018-07-25 22:36:38 -0700

    New network

--- resources/analysis.sql
+++ resources/analysis.sql
@@ -1,3 +1,6 @@
+with date as (
+
+)
 select *
 from dw.log
 where date between '2017-01-01' and '2017-12-31'
@@ -11,5 +14,3 @@ select * from crosstab (
    order by 1,2',
   'select distinct device from dw.log order by 1'
 )  as ct (  date varchar,firewall text,pfsense text,router text  );
-
-
